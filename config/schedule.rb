d5e7f72cedc8685b732382dfa1346e2c6bdb8966
Boggs <hello.boggs@gmail.com>
2016-02-08 01:12:07 +0800

    Added santolan and shaw to schedule config.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,12 +1,18 @@
 RUBY_EXECUTABLE = "/usr/bin/ruby"
 PROJECT_DIRECTORY = "~/dev/mrt-scraper"
 STORAGE_DIRECTORY = "~/dev/mrt-scraper/foobar"
-STATION_ID = "taft-avenue"
+STATION_IDS = [
+  "taft-avenue",
+  "santolan-anapolis",
+  "shaw-boulevard",
+]
 
-every 1.minute do
-  command "#{RUBY_EXECUTABLE} #{PROJECT_DIRECTORY}/mrt-scraper.rb -s #{STATION_ID} -d #{STORAGE_DIRECTORY}"
-end
+STATION_IDS.each { |station_id|
+  every 1.minute do
+    command "#{RUBY_EXECUTABLE} #{PROJECT_DIRECTORY}/mrt-scraper.rb -s #{station_id} -d #{STORAGE_DIRECTORY}"
+  end
 
-every 1.minute do
-  command "sleep 30 && #{RUBY_EXECUTABLE} #{PROJECT_DIRECTORY}/mrt-scraper.rb -s #{STATION_ID} -d #{STORAGE_DIRECTORY}"
-end
+  every 1.minute do
+    command "sleep 30 && #{RUBY_EXECUTABLE} #{PROJECT_DIRECTORY}/mrt-scraper.rb -s #{station_id} -d #{STORAGE_DIRECTORY}"
+  end
+}
